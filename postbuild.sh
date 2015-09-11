6abb469256ad36e2dd5c9e9e727fcd5b23dee454
loblao <12ksit@gmail.com>
2015-09-11 17:15:32 -0300

    Update postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -1,17 +1,24 @@
-@echo off
 cd built
 echo Deleting unneeded files...
-rd /Q /S bin 2>nul
-rd /Q /S direct\extensions 2>nul
-rd /Q /S etc 2>nul
-rd /Q /S models 2>nul
-rd /Q /S plugins 2>nul
-rd /Q /S python 2>nul
-del LICENSE 2>nul
-del ReleaseNotes 2>nul
+rm -rf bin
+rm -rf direct/extensions
+rm -rf etc
+rm -rf models
+rm -rf plugins
+rm -rf python
+rm LICENSE
+rm ReleaseNotes
 
 cd lib
-del libp3pystub.lib 2>nul
+rm libp3pystub.a
+
+mv _direct.a lib_panda3d_direct.a
+mv _core.a lib_panda3d_core.a
+mv egg.a lib_panda3d_egg.a
+mv fx.a lib_panda3d_fx.a
+mv ode.a lib_panda3d_ode.a
+mv physics.a lib_panda3d_physics.a
+mv interrogatedb.a lib_panda3d_interrogatedb.a
 
 cd ..
 cd ..
